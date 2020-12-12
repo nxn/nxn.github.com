76ed9857a4fd5a3517c6cf3ceaa23bb9e9cf4349
nxn <ernie@nxn.io>
2020-12-11 21:18:15 -0500

    theme palette

--- src/components/layouts/resume/theme.tsx
+++ src/components/layouts/resume/theme.tsx
@@ -1,4 +1,26 @@
 
 export const palette = {
-    
+    page: {
+        text: '#000',
+        background: '#fcfcfc'
+    },
+    header: {
+        text: '#222'
+    },
+    item: {
+        background: '#fff'
+    },
+    icon: {
+        fill: '#c6c6c6',
+        hover: '#EF545F'
+    },
+    link: {
+        text: '#000',
+        hover: '#CD546C'
+    },
+    accent: {
+        subtle: '#c6c6c6'
+    },
+    gray:   '#666666',
+    white:  '#ffffff'
 }
