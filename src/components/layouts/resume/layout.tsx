7d164b0d001f00d82e2ca30fddbdb548938a4f3e
nxn <ernie@nxn.io>
2020-12-16 10:57:18 -0500

    resume converted to mdx; still needs print fixes

--- src/components/layouts/resume/layout.tsx
+++ src/components/layouts/resume/layout.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-import Header   from "./header";
-import Footer   from "./footer";
-
 import theme from "../../theme";
 import { withGlobal } from "../../util";
 
 import '../../../styles/reset.css';
 import '../../../styles/fonts.css';
+
+import chevron from '../../../images/chevron.svg';
 
 type LayoutProps = {
     children?: React.ReactNode;
@@ -18,9 +17,7 @@ type LayoutProps = {
 export function LayoutUnstyled(props: LayoutProps) {
     return (
         <div id="page" className={ props.className }>
-            <Header />
             { props.children }
-            <Footer />
         </div>
     );
 }
@@ -46,27 +43,19 @@ const globalStyles = {
         }
     },
 
-    'h1, h2, h3, h4, h5, .title':   { color:        theme.resume.palette.header.text },
-    'h1, h3':                       { fontFamily:   '"Oswald", sans-serif' },
-    'h2, h4, h5, .title':           { fontFamily:   '"Roboto Slab", serif' },
-    'h5, .title':                   { fontWeight:   700 },
+    'h1, h2, h3, h4, h5, h6, strong':   { color:        theme.resume.palette.header.text },
+    'h1, h3':                           { fontFamily:   '"Oswald", sans-serif' },
+    'h2, h4, h5, h6, strong':           { fontFamily:   '"Roboto Slab", serif' },
+    'h5, strong':                       { fontWeight:   700 },
 
     h1: { 
         fontSize:   '3rem',
         lineHeight: '1.0em'
     },
     h2: {
-        fontSize:       '2rem',
-        margin:         '2rem 0 2rem 0',
-        display:        'flex',
-        alignItems:     'center',
-        justifyContent: 'center'
-    },
-    'h2 .icon': { 
-        height:     '1.6375rem',
-        width:      '2.0rem',
-        opacity:    '0.2',
-        margin:     '0.1rem 1rem 0 -3rem'
+        fontSize:   '2rem',
+        margin:     '2rem 0 2rem 0',
+        display:    'block'
     },
     h3: {
         fontSize:   '1.5rem',
@@ -78,14 +67,114 @@ const globalStyles = {
     },
     h5: {
         marginBottom: '0.5rem'
-    }
+    },
+    h6: {
+        fontSize:   '1.25rem',
+        lineHeight: '2rem'
+    },
+
+    p: {
+        marginBottom: '1.0rem'
+    },
+
+    em: { fontWeight: 400 },
+
+    ul: {
+        listStyleType:  'disc',
+        listStyleImage: `url("${ chevron }")`
+    },
+    li: {
+        paddingBottom:  '0.5rem',
+        breakInside:    'avoid' as 'avoid'
+    },
+
+    hr: {
+        margin:             '0.5rem 0',
+        borderStyle:        'solid',
+        borderBottomWidth:  '0.25rem',
+        borderTop:          0,
+        borderLeft:         0,
+        borderRight:        0,
+        borderImage: 
+            `linear-gradient(to right, ${ theme.resume.palette.accent.subtle }, rgba(247, 247, 247, 0)) 100% 1;`
+    },
 }
 
-export const Layout = styled(withGlobal(LayoutUnstyled, globalStyles))({
+export const Layout = styled(withGlobal(LayoutUnstyled, globalStyles))(({theme: { resume: theme }}) => ({
     maxWidth:   '64rem',
     minWidth:   '17.5rem', /* ~280px with 20px of padding on each side */
-    margin:     '0 auto'
-});
+    margin:     '0 auto',
+
+    '& #tech-tags ul': {
+        display:    'flex',
+        flexFlow:   'row wrap',
+
+        '& li': {
+            lineHeight:         '1.75em',
+            color:              theme.palette.page.text,
+            border:             `0.0625rem solid ${ theme.palette.accent.subtle }`,
+            backgroundColor:    theme.palette.item.background,
+            padding:            '0 0.6rem',
+            marginBottom:       '0.4rem',
+            marginRight:        '0.4rem',
+            borderRadius:       '0.2rem',
+            listStyle:          'none'
+        }
+    },
+
+    '& header h6': {
+        marginBottom: '1rem'
+    },
+
+    '& #summary': {
+        '& p': {
+            marginTop: '1rem'
+        },
+        '& h3': {
+            marginBottom: '0.5rem'
+        }
+    },
+
+    '& footer': {
+        color:      theme.palette.gray,
+        fontSize:   '0.9rem',
+        paddingTop: '1rem'
+    },
+
+    '@media screen and (min-width: 48rem)': {
+        '& section ul': {
+            columns:    2,
+            columnGap: '3rem'
+        },
+        '& #summary': {
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+            columnGap: '3rem',
+            gridTemplateAreas: `
+                "paragraph   paragraph"
+                "specialties tech-tags"
+            `,
+            '& p': {
+                gridArea: 'paragraph'
+            },
+            '& h3': {
+                marginTop: '0.5rem'
+            },
+            '& #specialties': {
+                gridArea: 'specialties'
+            },
+            '& tech-tags': {
+                gridArea: 'tech-tags'
+            }
+        }
+    },
+
+    '@media print': {
+        '& footer': {
+            display: 'none'
+        }
+    }
+}));
 
 export default Layout;
 
---
+++ src/components/resume/menu.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import graphics from "../../images/graphics.svg";
+
+type MenuProps = {
+    className?: string
+}
+
+export function MenuUnstyled(props: MenuProps) {
+    return (
+        <ul className={ props.className }>
+            <li className="doc">
+                <a className="pdf" href="<%= require('./downloads/ewieczorek.pdf').default %>" target="_blank">
+                    <svg className="icon">
+                        <use href={ `${ graphics }#icon-doc` } />
+                    </svg>
+                    Download PDF
+                </a>
+            </li>
+            <li className="email">
+                <svg className="icon"><use href={ `${ graphics }#icon-mail` } /></svg>
+                <svg className="address"><use href={ `${ graphics }#mail-address` } /></svg>
+            </li>
+            <li className="web">
+                <a href="/">
+                    <svg className="icon">
+                        <use href={ `${ graphics }#icon-web` } />
+                    </svg>
+                    www.nxn.io
+                </a>
+            </li>
+            <li className="location">
+                <svg className="icon">
+                    <use href={ `${ graphics }#icon-geo` } />
+                </svg>
+                Philadelphia, PA
+            </li>
+        </ul>
+    );
+}
+
+export const Menu = styled(MenuUnstyled)(({theme: { resume: theme }}) => ({
+    display:        'flex',
+    flexDirection:  'column',
+    listStyle:      'none',
+
+    '@media screen and (min-width: 48rem)': {
+        flexDirection:  'row',
+        justifyContent: 'space-between'
+    },
+
+    '@media print': {
+        border:     0,
+        padding:    0,
+        margin:     0,
+
+        '& li.doc': {
+            display: 'none'
+        }
+    },
+
+    '& li': {
+        textAlign: 'left',
+        lineHeight: '2.5rem',
+        padding: 0,
+
+        '@media print': {
+            lineHeight: '1.66rem',
+            height: '1.66rem'
+        },
+
+        '& .icon': {
+            display: 'inline-block',
+            height: '2.5rem',
+            width: '1.5rem',
+            verticalAlign: 'top',
+            fill: theme.palette.icon.fill,
+            marginRight: '0.5rem',
+
+            '@media print': {
+                lineHeight: '1.66rem',
+                height: '1.66rem'
+            },
+        },
+
+        '& a': {
+            color: theme.palette.header.link.text,
+            textDecoration: 'none',
+            display: 'inline-block', /* inline to not stretch in mobile mode */
+            paddingRight: '1rem'
+        },
+
+        '& a:hover': {
+            color: theme.palette.header.link.hover,
+            '& .icon': {
+                fill: theme.palette.icon.hover
+            }
+        }
+    },
+
+    '& .email .address': {
+        height:         '0.85rem',
+        width:          '6.4rem',
+        marginTop:      '-0.0625rem',
+        verticalAlign:  'middle',
+        fill:           theme.palette.page.text
+    }
+}));
+
+export default Menu;
