176f57e292fb0fda617fbb8043baa9d3f773113e
nxn <ernie@nxn.io>
2021-01-20 05:35:44 -0500

    v3

---
+++ component---src-pages-404-tsx-01902f16a1dd5f9815fb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(t,e,n){"use strict";n.r(e),n.d(e,"Error404Page",(function(){return l}));n("q1tI");var c=n("qhky"),a=n("C5yf"),u=n("0cdV"),i=n("AeFk");function l(t){var e=t.data.site.siteMetadata;return Object(i.c)(a.a,null,Object(i.c)(c.b,null,Object(i.c)("title",null,"Page not found · ",e.title)),Object(i.c)(u.a,null))}e.default=l}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-01902f16a1dd5f9815fb.js.map
